b1df239937acfb0de3f71685fefbf90f788a9a7c
David Murphy <dmurphy@saltstack.com>
2020-12-09 17:11:00 -0700

    Fix cffi 1.14.3 for Junos builds on FreeBSD 10.2, cannot handle gcc pragma

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ if platform.python_implementation() == "PyPy":
             "upgrade PyPy to use this library."
         )
 else:
-    requirements.append("cffi>=1.4.1")
-    setup_requirements.append("cffi>=1.4.1")
+    requirements.append("cffi==1.14.3")
+    setup_requirements.append("cffi==1.14.3")
 
 
 def here(*paths):
